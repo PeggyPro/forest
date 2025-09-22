1694e39cd4b28f93637dd96a83eff4606374449d
dt_flys <dt_flys@hotmail.com>
2025-09-22 13:30:41 +0800

    test: 当http client 做后端的时候，get 请求会拿不到带泛型的数据 (#243)

--- forest-core/src/test/java/com/dtflys/forest/test/TestGenericForestClient.java
+++ forest-core/src/test/java/com/dtflys/forest/test/TestGenericForestClient.java
@@ -2128,6 +2128,54 @@ public class TestGenericForestClient extends BaseClientTest {
         assertThat(user2.getEmail()).isEqualTo("foo@bar.com");
     }
 
+    public static class MyPost {
+        private Long userId;
+        private Long id;
+        private String title;
+        private String body;
+
+        public Long getUserId() {
+            return userId;
+        }
+
+        public void setUserId(Long userId) {
+            this.userId = userId;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public void setBody(String body) {
+            this.body = body;
+        }
+    }
+
+
+    @Test
+    public void testRequest_get_return_genericType() {
+        List<MyPost> posts = Forest.get("https://jsonplaceholder.typicode.com/posts")
+                .execute(new TypeReference<List<MyPost>>() {});
+        assertThat(posts).isNotNull();
+    }
+
+
 
     @Test
     public void testRequest_get_return_list2() {
